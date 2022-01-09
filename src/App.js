4e14f66c4b6e0cd286083cdd0be6cb5e800e3743
George Offley <geo.offley@gmail.com>
2022-01-09 16:35:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Goerge. I see you
+          Hello from Goerge. I see you2
         </p>
         <a
           className="App-link"
